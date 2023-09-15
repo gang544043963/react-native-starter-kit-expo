18859666d7fe2ac683ac1a991b17f5d5ac7aba3d
Li Gang <gang.li@thoughtworks.com>
2023-09-15 11:14:38 +0800

    add react-navigaton

--- App.tsx
+++ App.tsx
@@ -1,21 +1,6 @@
 import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import AppNavigator from './src/navigators/AppNavigator';
 
 export default function App() {
-    return (
-        <View style={styles.container}>
-            <Text>Open up App.js to start working on your app!</Text>
-            <StatusBar style="auto" />
-        </View>
-    );
+    return <AppNavigator />;
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center'
-    }
-});
